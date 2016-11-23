26df9ba788da52e05ad59bb20b55bafc0e0e2306
Fong Fan <fong.fan.90@gmail.com>
2016-11-23 00:39:55 -0800

    adds categories

--- process.rb
+++ process.rb
@@ -1,7 +1,107 @@
 require 'CSV'
 
+# Setup parameters
 file = './transactions.csv'
 start_date = "11/01/2016"
+
+# CONSTANTS
+### Groceries ###
+GROCERIES = [
+  "Groceries"
+]
+
+### Dining    ###
+DINING = [
+  "Alcohol & Bars",
+  "Coffee Shops",
+  "Fast Food",
+  "Food & Dining",
+  "Restaurants"
+]
+
+
+### Travel    ###
+TRAVEL = [
+  "Air Travel",
+  "Auto & Transport",
+  "Auto Insurance",
+  "Gas & Fuel",
+  "Parking",
+  "Public Transportation",
+  "Rental Car & Taxi",
+  "Travel"
+]
+
+### Purchases ###
+PURCHASES = [
+  "Amusement",
+  "Bonus",
+  "Books",
+  "Business Services",
+  "Check",
+  "Clothing",
+  "Dentist",
+  "Doctor",
+  "Electronics & Software",
+  "Entertainment",
+  "Federal Tax",
+  "Financial",
+  "Furnishings",
+  "Gift",
+  "Hair",
+  "Health Insurance",
+  "Home",
+  "Home Improvement",
+  "Home Services",
+  "Hotel",
+  "Internet",
+  "Legal",
+  "Mobile Phone",
+  "Movies & DVDs",
+  "Music",
+  "Newspapers & Magazines",
+  "Personal Care",
+  "Pets",
+  "Pharmacy",
+  "Printing",
+  "Service & Parts",
+  "Shopping",
+  "Sold Items",
+  "Sporting Goods",
+  "Sports"
+]
+
+### None      ###
+NONE = [
+  "ATM Fee",
+  "Auto Payment",
+  "Bank Fee",
+  "Cash & ATM",
+  "Charity",
+  "Credit Card Payment",
+  "Fees & Charges",
+  "Finance Charge",
+  "Financial Advisor",
+  "Gym",
+  "Income",
+  "Interest Income",
+  "Investments",
+  "Late Fee",
+  "Misc Expenses",
+  "Mortgage & Rent",
+  "Paycheck",
+  "Reimbursement",
+  "Service Fee",
+  "Shipping",
+  "State Tax",
+  "Stocks",
+  "Taxes",
+  "Transfer",
+  "Transfer for Cash Spending",
+  "Tuition",
+  "Uncategorized"
+]
+
 
 def main(file, start_date)
 
@@ -23,6 +123,7 @@ def read_file(file)
     description = row[1]
     amount = row[3]
     category = row[5]
+    transaction_type = row[4]
 
     # Additional Info
     original_description = row[2]
@@ -34,7 +135,8 @@ def read_file(file)
       amount: amount,
       category: category,
       original_description: original_description,
-      notes: notes
+      notes: notes,
+      transaction_type: transaction_type
     }
   end
   results
@@ -42,15 +144,13 @@ end
 
 def processing(result, start_date)
   internal_start_date = Date.strptime(start_date, '%m/%d/%Y')
-
-  if result[:category] == "Income" || result[:category] == "Paycheck"
-    result[:amount] = "-#{result[:amount]}"
-  end
-
   internal_transaction_date = Date.strptime(result[:date], '%m/%d/%Y')
-  # puts internal_transaction_date.inspect
 
   if internal_transaction_date > internal_start_date
+    if result[:transaction_type] == "credit"
+      result[:amount] = "-#{result[:amount]}"
+    end
+
     puts "#{result[:date]}|#{result[:description]}|#{result[:amount]}|#{result[:original_description]}|#{result[:notes]}"
   end
 end

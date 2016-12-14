84c3ec6fb0b9b6dd0969551595985b50ee91b579
Fong Fan <fong.fan.90@gmail.com>
2016-12-13 16:26:46 -0800

    extracts categories into a new file, and uses a hash

---
+++ categories.rb
@@ -0,0 +1,95 @@
+# Categories
+CATEGORIES = {
+  "Groceries": [
+    "Groceries"
+  ],
+  "Dining": [
+    "Alcohol & Bars",
+    "Coffee Shops",
+    "Fast Food",
+    "Food & Dining",
+    "Restaurants"
+  ],
+  "Travel": [
+    "Air Travel",
+    "Auto & Transport",
+    "Auto Insurance",
+    "Gas & Fuel",
+    "Parking",
+    "Public Transportation",
+    "Rental Car & Taxi",
+    "Travel"
+  ],
+  "Purchases": [
+    "Amusement",
+    "Bonus",
+    "Books",
+    "Business Services",
+    "Check",
+    "Clothing",
+    "Dentist",
+    "Doctor",
+    "Electronics & Software",
+    "Entertainment",
+    "Federal Tax",
+    "Financial",
+    "Furnishings",
+    "Gift",
+    "Hair",
+    "Health Insurance",
+    "Home",
+    "Home Improvement",
+    "Home Services",
+    "Hotel",
+    "Internet",
+    "Legal",
+    "Movies & DVDs",
+    "Music",
+    "Newspapers & Magazines",
+    "Personal Care",
+    "Pets",
+    "Pharmacy",
+    "Printing",
+    "Service & Parts",
+    "Shopping",
+    "Sold Items",
+    "Sporting Goods",
+    "Sports"
+  ],
+  "Recurring Purchases": [
+    "Gym",
+    "Mobile Phone"
+  ],
+  "Income": [
+    "Income",
+    "Mortgage & Rent",
+    "Interest Income",
+    "Paycheck",
+    "Reimbursement"
+  ],
+  "Banking": [
+    "Auto Payment",
+    "Investments",
+    "Transfer",
+    "Credit Card Payment"
+  ],
+  "None": [
+    "ATM Fee",
+    "Bank Fee",
+    "Cash & ATM",
+    "Charity",
+    "Fees & Charges",
+    "Finance Charge",
+    "Financial Advisor",
+    "Late Fee",
+    "Misc Expenses",
+    "Service Fee",
+    "Shipping",
+    "State Tax",
+    "Stocks",
+    "Taxes",
+    "Transfer for Cash Spending",
+    "Tuition",
+    "Uncategorized"
+  ]
+}
